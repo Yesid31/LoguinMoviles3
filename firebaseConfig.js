9302d08c062a48cc904779676c53ff5929f13a1d
Felipeoh23 <samuelmesarpo@gmail.com>
2023-10-04 06:28:45 -0500

    todo

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCCefetzp3MfRK3xul_KVctHlMJlAuUk78",
+  authDomain: "login-7c683.firebaseapp.com",
+  projectId: "login-7c683",
+  storageBucket: "login-7c683.appspot.com",
+  messagingSenderId: "609066082846",
+  appId: "1:609066082846:web:deda59dc351e5d7e72c40f"
+};
+
